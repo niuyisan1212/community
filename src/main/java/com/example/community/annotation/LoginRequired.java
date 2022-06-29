2f0f62a4db6b21b48ac94e6f610b8dccc37f76ab
niuhaoqing <1097960702@qq.com>
2022-06-29 13:16:43 +0800

    完善 登录功能

---
+++ src/main/java/com/example/community/annotation/LoginRequired.java
@@ -0,0 +1,16 @@
+package com.example.community.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @Author INSLYAB
+ * @Date 2022/6/29 13:04
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LoginRequired {
+
+}
---
+++ src/main/java/com/example/community/controller/UserController.java
@@ -0,0 +1,103 @@
+package com.example.community.controller;
+
+import com.example.community.annotation.LoginRequired;
+import com.example.community.entity.User;
+import com.example.community.service.UserService;
+import com.example.community.util.CommunityUtil;
+import com.example.community.util.HostHolder;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/**
+ * @Author INSLYAB
+ * @Date 2022/6/29 12:10
+ */
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @Value("${community.path.upload}")
+    private String uploadPath;
+
+    @Value("${community.path.domain}")
+    private String domain;
+
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private HostHolder hostHolder;
+
+    @LoginRequired
+    @RequestMapping(path = "/setting", method = RequestMethod.GET)
+    public String getSettingPage(){
+        return "/site/setting";
+    }
+
+    @LoginRequired
+    @RequestMapping(path = "/upload", method = RequestMethod.POST)
+    public String uploadHeader(MultipartFile headerImage, Model model){
+        if(headerImage == null){
+            model.addAttribute("error","您还没有选择图片");
+            return "/site/setting";
+        }
+        String fileName = headerImage.getOriginalFilename();
+        String suffix = fileName.substring(fileName.lastIndexOf("."));
+        if(StringUtils.isBlank(suffix)){
+            model.addAttribute("error","文件格式不正确");
+            return "/site/setting";
+        }
+        fileName = CommunityUtil.generateUUID() + suffix;
+        File dest = new File(uploadPath + "/" +fileName);
+        try {
+            //存储文件
+            headerImage.transferTo(dest);
+        } catch (IOException e) {
+            logger.error("上传文件失败: " + e.getMessage());
+            throw new RuntimeException("上传文件失败，服务器异常" + e.getMessage());
+        }
+        //更新用户头像路径
+        User user = hostHolder.getUser();
+        String headerUrl = domain + contextPath + "/user/header/" + fileName;
+        userService.updateHeader(user.getId(), headerUrl);
+        return "redirect:/index";
+    }
+
+    @RequestMapping(value = "/header/{fileName}", method = RequestMethod.GET)
+    public void getHeader(@PathVariable("fileName") String fileName,
+                          HttpServletResponse response){
+        fileName = uploadPath + "/" + fileName;
+        String suffix = fileName.substring(fileName.lastIndexOf("."));
+        response.setContentType("image/" + suffix);
+        try(FileInputStream fis = new FileInputStream(fileName);
+            ServletOutputStream os = response.getOutputStream();) {
+            byte[] buffer = new byte[1024];
+            int b = 0;
+            while((b = fis.read(buffer)) != -1){
+                os.write(buffer, 0, b);
+            }
+        } catch (IOException e) {
+            logger.error("读取头像失败：" + e.getMessage());
+        }
+    }
+}
