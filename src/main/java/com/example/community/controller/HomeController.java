ae932edced7bbc362abcc4c94a6217de19cef925
niuhaoqing <1097960702@qq.com>
2022-07-06 12:54:19 +0800

    新增 统一处理异常

--- src/main/java/com/example/community/controller/HomeController.java
+++ src/main/java/com/example/community/controller/HomeController.java
@@ -50,4 +50,8 @@ public class HomeController {
         return "/index";
     }
 
+    @RequestMapping(value = "/error", method = RequestMethod.GET)
+    public String getErrorPage(){
+        return "/error/500";
+    }
 }
